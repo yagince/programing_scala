2087bf7ef81264ed8fe10ffada745a2039f6bed8
Natsuki Yagi <straitwalk@gmail.com>
2011-12-08 20:35:07 +0900

    Stackのサンプル

---
+++ src/collection/sequence/StackSamples.scala
@@ -0,0 +1,23 @@
+package collection.sequence
+import scala.collection.mutable.Stack
+import scala.collection.immutable.{Stack => ImmutableStack}
+
+object StackSamples {
+  def main(args: Array[String]) {
+	val stack = new Stack[Int]
+	stack.push(1)
+	stack.push(2)
+	println("top : " + stack.top)
+	println("pop : " + stack.pop)
+	println("pop : " + stack.pop)
+	
+	val imStack = new ImmutableStack[Int]
+	val has1 = imStack.push(1)
+	println("imStack : " + imStack)
+	val has21 = has1.push(2)
+	val has321 = has21.push(3)
+	println(has321.pop2._1)
+	println(has321.pop.pop2._1)
+	println(has321.pop.pop.pop2._1)
+  }
+}
