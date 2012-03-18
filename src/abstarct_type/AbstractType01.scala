efc89d6db3adff8700c1a22c0615868aa90894e5
Natsuki Yagi <straitwalk@gmail.com>
2012-03-18 13:35:35 +0900

    抽象型

---
+++ src/abstarct_type/AbstractType01.scala
@@ -0,0 +1,19 @@
+package cupselize
+
+object AbstractType01 {
+  def main(args: Array[String]) {
+	new Cow eat (new Grass)
+  }
+}
+
+class Food
+abstract class Animal {
+  type SuitableFood <: Food // <: は上限境界
+  def eat(food: SuitableFood)
+}
+
+class Grass extends Food
+class Cow extends Animal {
+  type SuitableFood = Grass
+  override def eat(food: Grass) {}
+}
